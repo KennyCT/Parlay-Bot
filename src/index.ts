d058b07133b81ab9919efae287f3bc15487060d0
Kenny Contreras <kennycontrerastan@gmail.com>
2024-03-08 02:29:57 -0500

    Additional Foundation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Bot Test");
+import 'dotenv/config';
+import { Client } from 'discord.js';
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent'],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.username} is online.`);
+});
+
+client.login(process.env.TOKEN);
